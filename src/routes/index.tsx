6dd00483e5cd9ef36e6ec0d67e35545818980c0d
Brian Lucca <brianlucca2002@gmail.com>
2026-08-31 13:35:21 -0300

    refactor(voice): remove prop de visibilidade da transmissão

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -409,7 +409,6 @@ function Index() {
               screenShareEnabled={voice.screenShareEnabled}
               screenShares={voice.screenShares}
               localPreview={voice.localPreview}
-              isTabHidden={voice.isTabHidden}
               onToggleMic={handleToggleMic}
               onToggleScreenShare={handleToggleScreenShare}
               onLeave={handleLeaveVoice}
